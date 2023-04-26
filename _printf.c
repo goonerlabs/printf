6ff555eed588e412ca3dd614c2f70e60771e5204
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-25 20:09:12 -0700

    add check fro if get_func returns NULL

--- _printf.c
+++ _printf.c
@@ -33,6 +33,8 @@ int _printf(const char *format, ...)
 		}
 
 		f = get_func(format[i + 1]);
+		if (f == NULL)
+			return (-1);
 		if (format[i + 1] == 'c' || format[i + 1] == 'd' || format[i + 1] == 'i' ||
 				format[i + 1] == 'b' ||	format[i + 1] == 'o' || format[i + 1] == 'u')
 		{
